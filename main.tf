e4550795a88a01e8df204708ae66a8ef98877af8
quentinsvn <quentin.savean@gmail.com>
2024-02-12 11:42:28 +0100

    MAJ

--- main.tf
+++ main.tf
@@ -45,3 +45,24 @@ resource "azurerm_application_insights" "insights01" {
   workspace_id        = azurerm_log_analytics_workspace.loganalystics01.id
   application_type    = "web"
 }
+
+resource "azurerm_key_vault" "kv01" {
+  name                        = "kv-${var.project_name}-01"
+  location                    = azurerm_resource_group.app01.location
+  resource_group_name         = azurerm_resource_group.app01.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "webapp01" {
+  key_vault_id = azurerm_key_vault.kv01.id
+  tenant_id    = azurerm_linux_web_app.webapp01.identity[0].tenant_id
+  object_id    = azurerm_linux_web_app.webapp01.identity[0].principal_id
+
+  secret_permissions = [
+    "Get"
+  ]
+}
